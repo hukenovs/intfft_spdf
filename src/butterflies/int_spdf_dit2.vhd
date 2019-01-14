54dc8ff219dd97e7517cca63f9d1e654db48818c
Alexander Kapitanov <sallador@bk.ru>
2019-01-14 19:35:13 +0300

    Add SPDF butterflies: DIT / DIF Radix-2

--- src/butterflies/int_spdf_dit2.vhd
+++ src/butterflies/int_spdf_dit2.vhd
@@ -1,6 +1,6 @@
 -------------------------------------------------------------------------------
 --
--- Title       : int_spdf_dif2
+-- Title       : int_spdf_dit2
 -- Design      : FFT SPDF
 -- Author      : Kapitanov Alexander
 -- Company     : 
@@ -10,9 +10,9 @@
 --
 -- Version 1.0 : 11.01.2019
 --
--- Description : Single-Path Delay-Feedback butterfly Radix-2 (DIF*)
+-- Description : Single-Path Delay-Feedback butterfly Radix-2 (DIT*)
 --
--- * - Decimation in frequency. 2 points per clock.
+-- * - Decimation in time. 2 points per clock.
 --
 -- Algorithm: Decimation in frequency
 --
@@ -56,7 +56,7 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-entity int_spdf_dif2 is
+entity int_spdf_dit2 is
     generic (
         FORMAT     : integer:=1; --! 1 - Uscaled, 0 - Scaled
         RNDMODE    : string:="TRUNCATE"; --integer:=0; --! 0 - Truncate, 1 - Rounding (FORMAT should be = 0)
@@ -79,9 +79,9 @@ entity int_spdf_dif2 is
         RST        : in  std_logic; --! Global Reset
         CLK        : in  std_logic --! DSP Clock
     );
-end int_spdf_dif2;
+end int_spdf_dit2;
 
-architecture int_spdf_dif2 of int_spdf_dif2 is
+architecture int_spdf_dit2 of int_spdf_dit2 is
 
 function fn_twd_delay(ivar : integer) return integer is
     variable ret_val   : integer;
@@ -105,101 +105,35 @@ signal das_re      : std_logic_vector(DATA_WIDTH downto 0);
 signal das_im      : std_logic_vector(DATA_WIDTH downto 0);
 signal das_vl      : std_logic;
 
-signal fmt_re      : std_logic_vector(DATA_WIDTH+FORMAT-1 downto 0);
-signal fmt_im      : std_logic_vector(DATA_WIDTH+FORMAT-1 downto 0);
-signal fmt_vl      : std_logic;
-
 signal ww_re       : std_logic_vector(TWDL_WIDTH-1 downto 0);
 signal ww_im       : std_logic_vector(TWDL_WIDTH-1 downto 0);
 
+signal mlt_re      : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal mlt_im      : std_logic_vector(DATA_WIDTH-1 downto 0);
+signal mlt_vl      : std_logic;
+
 begin
 
-    ----------------------------------------------------------------------------
-    -- Select output format after Add/Sub operation ----------------------------
-    ----------------------------------------------------------------------------
-    xROUND: if ((FORMAT = 0) and (RNDMODE = "ROUNDING")) generate
-    begin   
-        ---- Rounding mode: +/- 0.5 ----
-        pr_rnd: process(clk) is
-        begin
-            if rising_edge(clk) then
-                fmt_vl <= das_vl;
-                if (das_re(0) = '0') then
-                    fmt_re <= das_re(DATA_WIDTH downto 1);
-                else
-                    fmt_re <= das_re(DATA_WIDTH downto 1) + '1';
-                end if;
-                if (das_im(0) = '0') then
-                    fmt_im <= das_im(DATA_WIDTH downto 1);
-                else
-                    fmt_im <= das_im(DATA_WIDTH downto 1) + '1';
-                end if;
-            end if;
-        end process;
-    end generate;
-
-    xTRUNCATE: if ((FORMAT = 0) and (RNDMODE = "TRUNCATE")) generate
-    begin   
-        --pr_rnd: process(clk) is
-        --begin
-        --    if rising_edge(clk) then
-                fmt_re <= das_re(DATA_WIDTH downto 1-FORMAT);
-                fmt_im <= das_im(DATA_WIDTH downto 1-FORMAT);
-                fmt_vl <= das_vl;
-        --    end if;
-        --end process;
-    end generate;
-
-    xUNSCALED: if (FORMAT = 1) generate
-    begin
-        fmt_re <= das_re(DATA_WIDTH downto 1-FORMAT);
-        fmt_im <= das_im(DATA_WIDTH downto 1-FORMAT);
-        fmt_vl <= das_vl;
-    end generate;
-
-    ----------------------------------------------------------------------------
-    -- Add/Sub & Delay-Feedback ------------------------------------------------
-    ----------------------------------------------------------------------------
-    xSPDF_ADDSUB: entity work.int_fly_addsub
-        generic map (
-            STAGE        => STAGE,
-            NFFT         => NFFT,
-            DTW          => DATA_WIDTH,
-            XUSE         => XUSE,
-            XSER         => XSER
-        )
-        port map (
-            RST          => RST,
-            CLK          => CLK,
-
-            DI_RE        => DI_RE,
-            DI_IM        => DI_IM,
-            DI_EN        => DI_EN,
-            
-            DO_RE        => das_re,
-            DO_IM        => das_im,
-            DO_VL        => das_vl
-        );
     ----------------------------------------------------------------------------
     -- Align data and twiddles -------------------------------------------------
     ----------------------------------------------------------------------------
     pr_del: process(clk) is
     begin
         if rising_edge(clk) then
-            dre_zz <= dre_zz(FTWDL-2 downto 0) & fmt_re;
-            dim_zz <= dim_zz(FTWDL-2 downto 0) & fmt_im;
-            ena_zz <= ena_zz(FTWDL-2 downto 0) & fmt_vl;
+            dre_zz <= dre_zz(FTWDL-2 downto 0) & DI_RE;
+            dim_zz <= dim_zz(FTWDL-2 downto 0) & DI_IM;
+            ena_zz <= ena_zz(FTWDL-2 downto 0) & DI_EN;
         end if;
     end process;
 
     ----------------------------------------------------------------------------
-    -- Complex multiplier and delay --------------------------------------------
+    -- Complex multiplier and input data ---------------------------------------
     ----------------------------------------------------------------------------
     xSPDF_TWDLS: entity work.int_fly_twd
         generic map (
             STAGE        => STAGE,
             NFFT         => NFFT,
-            DTW          => DATA_WIDTH+FORMAT,
+            DTW          => DATA_WIDTH,
             XSER         => XSER
         )
         port map (
@@ -213,9 +147,9 @@ begin
             WW_RE        => ww_re,
             WW_IM        => ww_im,
             
-            DO_RE        => DO_RE,
-            DO_IM        => DO_IM,
-            DO_VL        => DO_VL
+            DO_RE        => mlt_re,
+            DO_IM        => mlt_im,
+            DO_VL        => mlt_vl
         );
 
     ----------------------------------------------------------------------------
@@ -232,9 +166,76 @@ begin
         port map (
             CLK      => CLK,
             RST      => rst,
-            WW_EN    => das_vl,
+            WW_EN    => di_en,
             WW_RE    => ww_re,
             WW_IM    => ww_im
         );
 
-end int_spdf_dif2;
+    ----------------------------------------------------------------------------
+    -- Add/Sub & Delay-Feedback ------------------------------------------------
+    ----------------------------------------------------------------------------
+    xSPDF_ADDSUB: entity work.int_fly_addsub
+        generic map (
+            STAGE        => STAGE,
+            NFFT         => NFFT,
+            DTW          => DATA_WIDTH,
+            XUSE         => XUSE,
+            XSER         => XSER
+        )
+        port map (
+            RST          => RST,
+            CLK          => CLK,
+
+            DI_RE        => mlt_re,
+            DI_IM        => mlt_im,
+            DI_EN        => mlt_vl,
+            
+            DO_RE        => das_re,
+            DO_IM        => das_im,
+            DO_VL        => das_vl
+        );
+
+    ----------------------------------------------------------------------------
+    -- Select output format after Add/Sub operation ----------------------------
+    ----------------------------------------------------------------------------
+    xROUND: if ((FORMAT = 0) and (RNDMODE = "ROUNDING")) generate
+    begin   
+        ---- Rounding mode: +/- 0.5 ----
+        pr_rnd: process(clk) is
+        begin
+            if rising_edge(clk) then
+                DO_VL <= das_vl;
+                if (das_re(0) = '0') then
+                    DO_RE <= das_re(DATA_WIDTH downto 1);
+                else
+                    DO_RE <= das_re(DATA_WIDTH downto 1) + '1';
+                end if;
+                if (das_im(0) = '0') then
+                    DO_IM <= das_im(DATA_WIDTH downto 1);
+                else
+                    DO_IM <= das_im(DATA_WIDTH downto 1) + '1';
+                end if;
+            end if;
+        end process;
+    end generate;
+
+    xTRUNCATE: if ((FORMAT = 0) and (RNDMODE = "TRUNCATE")) generate
+    begin   
+        --pr_rnd: process(clk) is
+        --begin
+        --    if rising_edge(clk) then
+                DO_RE <= das_re(DATA_WIDTH downto 1-FORMAT);
+                DO_IM <= das_im(DATA_WIDTH downto 1-FORMAT);
+                DO_VL <= das_vl;
+        --    end if;
+        --end process;
+    end generate;
+
+    xUNSCALED: if (FORMAT = 1) generate
+    begin
+        DO_RE <= das_re(DATA_WIDTH downto 1-FORMAT);
+        DO_IM <= das_im(DATA_WIDTH downto 1-FORMAT);
+        DO_VL <= das_vl;
+    end generate;
+
+end int_spdf_dit2;
